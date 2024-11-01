8936b549f7158c60add89bafd834d1459bc30547
Leo <leo.petrov.92@yandex.ru>
2024-11-01 12:14:10 +0300

    number, array

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 console.log("Hello, World");
 console.log("Hello, St-Peterburg");
 
+//Number и операторы
 let num1 = 10;
 let num2 = 5;
 
 let num = num1 ** num2;
 
-console.log(num);
+let x = 4;
+let y = "4";
+
+console.log(x === y);
+
+//array
+
+let arr1 = new Array(10);
+let arr2 = ["yellow", "green", "black", "grey"];
+
+console.log(arr1);
+console.log(arr2);
